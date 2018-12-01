fbb05fc140a508771c8aa79cedc3efccb3ba6753
Guido Knips <guido.knips@posteo.de>
2018-12-01 17:33:18 +0100

    add reading file

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::env;
+
+fn main() -> Result<(), String> {
+    let filename = env::args().nth(1).ok_or("No file name given.".to_owned())?;
+    let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
+fn read_file(path: &Path) -> std::io::Result<String> {
+    let ifile = File::open(path)?;
+    let mut bufr = BufReader::new(ifile);
+    let mut result = String::with_capacity(2048);
+    bufr.read_to_string(&mut result)?;
+    return Ok(result);
+}
