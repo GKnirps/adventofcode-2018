797eb99a2cb06ebd3522070a8119788257383734
Guido Knips <guido.knips@posteo.de>
2018-12-01 18:06:14 +0100

    add solution for puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, Read};
 use std::env;
+use std::collections::HashSet;
 
 fn main() -> Result<(), String> {
     let filename = env::args().nth(1).ok_or("No file name given.".to_owned())?;
@@ -9,6 +10,9 @@ fn main() -> Result<(), String> {
     
     let puzzle1_result = puzzle1(&content);
     println!("result: {}", puzzle1_result);
+
+    let puzzle2_result = puzzle2(&content);
+    println!("result: {}", puzzle2_result);
 
     Ok(())
 }
@@ -25,15 +29,37 @@ fn puzzle1(input: &str) -> i64 {
     input.split('\n').filter_map(|s| s.parse::<i64>().ok()).sum()
 }
 
+fn puzzle2(input: &str) -> i64 {
+    let mut current: i64 = 0;
+    let mut seen: HashSet<i64> = HashSet::with_capacity(100);
+    seen.insert(current);
+    for n in input.split('\n').filter_map(|s| s.parse::<i64>().ok()).cycle() {
+        current = current + n;
+        if seen.contains(&current) {
+            return current;
+        }
+        seen.insert(current);
+    }
+    return 0;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn puzzle1_correctly() {
-        assert_eq!(calc(""), 0);
-        assert_eq!(calc("+1\n+2\n+3"), 6);
-        assert_eq!(calc("+1\n+2\n-3"), 0);
-        assert_eq!(calc("+1\n+2\n-4\n"), -1);
+        assert_eq!(puzzle1(""), 0);
+        assert_eq!(puzzle1("+1\n+2\n+3"), 6);
+        assert_eq!(puzzle1("+1\n+2\n-3"), 0);
+        assert_eq!(puzzle1("+1\n+2\n-4\n"), -1);
+    }
+
+    #[test]
+    fn puzzle2_correctly() {
+        assert_eq!(puzzle2("+1\n-1"), 0);
+        assert_eq!(puzzle2("+3\n+3\n+4\n-2\n-4"), 10);
+        assert_eq!(puzzle2("-6\n+3\n+8\n+5\n-6\n"), 5);
+        assert_eq!(puzzle2("+7\n+7\n-2\n-7\n-4\n"), 14);
     }
 }
