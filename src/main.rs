5fe9d194361077350eec99bbe5cdeac4df8a5fdb
Guido Knips <guido.knips@posteo.de>
2018-12-01 17:47:39 +0100

    add solution for puzzle 1

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::env;
 fn main() -> Result<(), String> {
     let filename = env::args().nth(1).ok_or("No file name given.".to_owned())?;
     let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
+    
+    let puzzle1_result = puzzle1(&content);
+    println!("result: {}", puzzle1_result);
 
     Ok(())
 }
@@ -16,4 +19,21 @@ fn read_file(path: &Path) -> std::io::Result<String> {
     let mut result = String::with_capacity(2048);
     bufr.read_to_string(&mut result)?;
     return Ok(result);
+}
+
+fn puzzle1(input: &str) -> i64 {
+    input.split('\n').filter_map(|s| s.parse::<i64>().ok()).sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn puzzle1_correctly() {
+        assert_eq!(calc(""), 0);
+        assert_eq!(calc("+1\n+2\n+3"), 6);
+        assert_eq!(calc("+1\n+2\n-3"), 0);
+        assert_eq!(calc("+1\n+2\n-4\n"), -1);
+    }
 }
