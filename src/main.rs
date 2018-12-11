a83758c0fe1ec6f6868ac58be0e099c44140a79c
Guido Knips <guido.knips@posteo.de>
2018-12-11 06:47:21 +0100

    add solution for puzzle 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,55 @@
+static GRID_SIDE: i32 = 300;
+
 fn main() -> Result<(), String> {
     let puzzle_input: i32 = 5177;
 
+    let grid = power_grid(puzzle_input);
+    let (xmax, ymax) = max_square(&grid);
+
+    println!("Position of highest power: {}×{}", xmax, ymax);
+
     Ok(())
+}
+
+fn empty_grid() -> Vec<i32> {
+    (0..(GRID_SIDE * GRID_SIDE)).map(|_| 0).collect()
+}
+
+fn power_grid(serial: i32) -> Vec<i32> {
+    let mut grid = empty_grid();
+    for y in 1..GRID_SIDE + 1 {
+        for x in 1..(GRID_SIDE + 1) {
+            grid[index(x, y)] = power_level(x, y, serial);
+        }
+    }
+    return grid;
+}
+
+fn max_square(grid: &[i32]) -> (i32, i32) {
+    let mut max: i32 = i32::min_value();
+    let mut pos: (i32, i32) = (1, 1);
+    for x in 1..GRID_SIDE - 1 {
+        for y in 1..(GRID_SIDE - 1) {
+            let power = grid[index(x, y)]
+                + grid[index(x + 1, y)]
+                + grid[index(x + 2, y)]
+                + grid[index(x, y + 1)]
+                + grid[index(x + 1, y + 1)]
+                + grid[index(x + 2, y + 1)]
+                + grid[index(x, y + 2)]
+                + grid[index(x + 1, y + 2)]
+                + grid[index(x + 2, y + 2)];
+            if power > max {
+                max = power;
+                pos = (x, y);
+            }
+        }
+    }
+    return pos;
+}
+
+fn index(x: i32, y: i32) -> usize {
+    ((x - 1) + (y - 1) * GRID_SIDE) as usize
 }
 
 fn power_level(x: i32, y: i32, serial: i32) -> i32 {
