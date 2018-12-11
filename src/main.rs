d8ebf9a84699d82e92f188900fb9ad47476057a6
Guido Knips <guido.knips@posteo.de>
2018-12-11 06:16:55 +0100

    add power level calculation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-use std::env;
-use std::fs::File;
-use std::io::{BufReader, Read};
-use std::path::Path;
-
 fn main() -> Result<(), String> {
-    let filename = env::args().nth(1).ok_or("No file name given.".to_owned())?;
-    let content = read_file(&Path::new(&filename)).map_err(|e| e.to_string())?;
-    let lines: Vec<&str> = content.split('\n').collect();
+    let puzzle_input: i32 = 5177;
 
     Ok(())
 }
 
-fn read_file(path: &Path) -> std::io::Result<String> {
-    let ifile = File::open(path)?;
-    let mut bufr = BufReader::new(ifile);
-    let mut result = String::with_capacity(2048);
-    bufr.read_to_string(&mut result)?;
-    return Ok(result);
+fn power_level(x: i32, y: i32, serial: i32) -> i32 {
+    let rack_id = x + 10;
+    let base_power = (y * rack_id + serial) * rack_id;
+    (base_power % 1000) / 100 - 5
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn power_level_work_for_examples() {
+        assert_eq!(power_level(3, 5, 8), 4);
+        assert_eq!(power_level(122, 79, 57), -5);
+        assert_eq!(power_level(217, 196, 39), 0);
+        assert_eq!(power_level(101, 153, 71), 4);
+    }
 }
