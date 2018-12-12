11cbac842ad9bd5735dc05242bca863b2df1dbdb
Guido Knips <guido.knips@posteo.de>
2018-12-12 06:45:49 +0100

    ot add input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate lazy_static;
+use regex::Regex;
+use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read};
@@ -19,7 +24,90 @@ fn read_file(path: &Path) -> std::io::Result<String> {
     return Ok(result);
 }
 
+struct State {
+    pots: VecDeque<bool>,
+    offset_left: usize,
+}
+
+fn parse_initial_state(line: &str) -> Option<State> {
+    lazy_static! {
+        static ref RE_STATE: Regex = Regex::new(r"initial state: ([.#]+)").unwrap();
+    }
+    let capture = RE_STATE.captures(line)?;
+    let pot_string = capture.get(1)?.as_str();
+    let pots: VecDeque<bool> = pot_string.chars().map(|c| c == '#').collect();
+    Some(State {
+        pots,
+        offset_left: 0,
+    })
+}
+
+type Rules = HashMap<[bool; 5], bool>;
+
+fn parse_rules(lines: &[&str]) -> Rules {
+    lines.iter().filter_map(|line| parse_rule(line)).collect()
+}
+
+fn parse_rule(line: &str) -> Option<([bool; 5], bool)> {
+    lazy_static! {
+        static ref RE_RULE: Regex = Regex::new(r"([.#]{5}) => ([.#])").unwrap();
+    }
+    let capture = RE_RULE.captures(line)?;
+    let mut con_it = capture.get(1)?.as_str().chars().map(|c| c == '#');
+    let condition: [bool; 5] = [
+        con_it.next()?,
+        con_it.next()?,
+        con_it.next()?,
+        con_it.next()?,
+        con_it.next()?,
+    ];
+    let result = capture.get(2)?.as_str().chars().next()? == '#';
+
+    return Some((condition, result));
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn parse_initial_state_works_correctly() {
+        // given
+        let line = "initial state: ..###...#foo";
+
+        // when
+        let state = parse_initial_state(line).expect("Expected successful parsing");
+
+        // then
+        assert_eq!(
+            &state.pots,
+            &[false, false, true, true, true, false, false, false, true]
+        );
+        assert_eq!(state.offset_left, 0);
+    }
+
+    #[test]
+    fn parse_rule_parses_rule() {
+        assert_eq!(
+            parse_rule(".#.#. => #"),
+            Some(([false, true, false, true, false], true))
+        );
+        assert_eq!(
+            parse_rule("#.#.# => ."),
+            Some(([true, false, true, false, true], false))
+        );
+    }
+
+    #[test]
+    fn parse_rules_parses_rules() {
+        // given
+        let lines = &[".#.#. => #", "#.#.# => ."];
+
+        // when
+        let rules = parse_rules(lines);
+
+        // then
+        assert_eq!(rules.len(), 2);
+        assert_eq!(rules.get(&[false, true, false, true, false]), Some(&true));
+    }
 }
