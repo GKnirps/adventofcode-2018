e4617f4caa8e8a0036ea72ed3b18ff7c4c3094ad
Guido Knips <guido.knips@posteo.de>
2018-12-14 07:38:43 +0100

    add solution for puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let last_scores = create_recipes(380621);
     println!("The last 10 scores are {:?}", last_scores);
+
+    let recipes_until_pattern = create_recipes_until_pattern(&[3, 8, 0, 6, 2, 1]);
+    println!(
+        "The pattern occurs after {} recipes.",
+        recipes_until_pattern
+    );
 }
 
 fn step(state: State) -> State {
@@ -33,6 +39,24 @@ fn create_recipes(n: usize) -> Vec<u8> {
     return state.scoreboard[n..n + 10].to_vec();
 }
 
+fn create_recipes_until_pattern(pattern: &[u8]) -> usize {
+    let mut state: State = State::with_capacity(2048);
+    loop {
+        state = step(state);
+        let score_len = state.scoreboard.len();
+        if score_len >= pattern.len()
+            && &state.scoreboard[score_len - pattern.len()..score_len] == pattern
+        {
+            return score_len - pattern.len();
+        }
+        if score_len >= pattern.len() + 1
+            && &state.scoreboard[score_len - 1 - pattern.len()..score_len - 1] == pattern
+        {
+            return score_len - pattern.len() - 1;
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct State {
     scoreboard: Vec<u8>,
@@ -63,5 +87,13 @@ mod test {
         assert_eq!(create_recipes(5), vec![0, 1, 2, 4, 5, 1, 5, 8, 9, 1]);
         assert_eq!(create_recipes(18), vec![9, 2, 5, 1, 0, 7, 1, 0, 8, 5]);
         assert_eq!(create_recipes(2018), vec![5, 9, 4, 1, 4, 2, 9, 8, 8, 2]);
+    }
+
+    #[test]
+    fn create_recipes_until_pattern_works_for_examples() {
+        assert_eq!(create_recipes_until_pattern(&[5, 1, 5, 8, 9]), 9);
+        assert_eq!(create_recipes_until_pattern(&[0, 1, 2, 4, 5]), 5);
+        assert_eq!(create_recipes_until_pattern(&[9, 2, 5, 1, 0]), 18);
+        assert_eq!(create_recipes_until_pattern(&[5, 9, 4, 1, 4]), 2018);
     }
 }
