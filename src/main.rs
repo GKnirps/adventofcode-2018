a01698eb4e3125cf67a54a48c1cd039e80a22bb5
Guido Knips <guido.knips@posteo.de>
2018-12-16 02:38:41 +0100

    WIP: add solution for puzzle 2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::path::Path;
 
-static ATTACK_POWER: u32 = 3;
+static GOB_ATTACK_POWER: u32 = 3;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Side {
@@ -97,7 +97,7 @@ impl Cavern {
 
     fn set(&mut self, x: usize, y: usize, tile: Tile) {
         if x < self.xs && y < self.ys {
-            self.tiles[x + y * self.ys] = tile;
+            self.tiles[x + y * self.xs] = tile;
         }
     }
 
@@ -221,12 +221,33 @@ fn shortest_path_to_enemy(
     }
     // backtrack the path
     let mut prev_pos: Option<(usize, usize)> = None;
-    let mut current_pos = get_best_candidate(&candidates, read_order)?;
+    let (target_x, target_y) = get_best_candidate(&candidates, read_order)?;
+    let mut current_pos = get_attack_side_for_candidate(target_x, target_y, &visited)?;
     while let Some(next_pos) = visited.get(&current_pos).and_then(|p| *p) {
         prev_pos = Some(current_pos);
         current_pos = next_pos;
     }
     return prev_pos;
+}
+
+fn get_attack_side_for_candidate(
+    x: usize,
+    y: usize,
+    visited: &HashMap<(usize, usize), Option<(usize, usize)>>,
+) -> Option<(usize, usize)> {
+    if y > 0 && visited.contains_key(&(x, y - 1)) {
+        return Some((x, y - 1));
+    }
+    if x > 0 && visited.contains_key(&(x - 1, y)) {
+        return Some((x - 1, y));
+    }
+    if visited.contains_key(&(x + 1, y)) {
+        return Some((x + 1, y));
+    }
+    if visited.contains_key(&(x, y + 1)) {
+        return Some((x, y + 1));
+    }
+    return None;
 }
 
 fn get_best_candidate(
@@ -265,21 +286,31 @@ fn best_target_in_range(
     return get_best_candidate(&lowest_health_enemies, entities);
 }
 
-fn next_round(mut cavern: Cavern) -> (Cavern, bool) {
+fn next_round(mut cavern: Cavern, elf_attack_power: u32) -> (Cavern, bool) {
     let entities = cavern.entities_in_reading_order();
 
     for (x, y, _) in &entities {
-        if cavern.n_goblins == 0 || cavern.n_elves == 0 {
-            return (cavern, false);
-        }
+        let attack_power = if let Some(entity) = cavern.get_entity(*x, *y) {
+            // only end the round prematurely if the unit looking for an enemy is still alive
+            if cavern.n_goblins == 0 || cavern.n_elves == 0 {
+                return (cavern, false);
+            }
+            match entity.side {
+                Side::Elf => elf_attack_power,
+                Side::Goblin => GOB_ATTACK_POWER,
+            }
+        } else {
+            // unit is dead
+            continue;
+        };
         if let Some((target_x, target_y)) = best_target_in_range(&cavern, &entities, *x, *y) {
-            attack_tile(&mut cavern, target_x, target_y);
+            attack_tile(&mut cavern, target_x, target_y, attack_power);
         } else if let Some((next_x, next_y)) = shortest_path_to_enemy(*x, *y, &cavern, &entities) {
             cavern.move_entity(*x, *y, next_x, next_y);
             if let Some((target_x, target_y)) =
                 best_target_in_range(&cavern, &entities, next_x, next_y)
             {
-                attack_tile(&mut cavern, target_x, target_y);
+                attack_tile(&mut cavern, target_x, target_y, attack_power);
             }
         }
     }
@@ -287,9 +318,9 @@ fn next_round(mut cavern: Cavern) -> (Cavern, bool) {
     return (cavern, true);
 }
 
-fn attack_tile(cavern: &mut Cavern, target_x: usize, target_y: usize) {
+fn attack_tile(cavern: &mut Cavern, target_x: usize, target_y: usize, attack_power: u32) {
     if let Some(enemy) = cavern.get_entity(target_x, target_y) {
-        if enemy.health <= ATTACK_POWER {
+        if enemy.health <= attack_power {
             match enemy.side {
                 Side::Elf => cavern.n_elves -= 1,
                 Side::Goblin => cavern.n_goblins -= 1,
@@ -302,18 +333,18 @@ fn attack_tile(cavern: &mut Cavern, target_x: usize, target_y: usize) {
                 Tile::Open(Some(Entity {
                     id: enemy.id,
                     side: enemy.side,
-                    health: enemy.health - ATTACK_POWER,
+                    health: enemy.health - attack_power,
                 })),
             );
         }
     }
 }
 
-fn fight(mut cavern: Cavern) -> (Cavern, u32) {
+fn fight(mut cavern: Cavern, elf_attack_power: u32) -> (Cavern, u32) {
     let mut rounds: u32 = 0;
     let mut round_finished = true;
     while round_finished {
-        let result = next_round(cavern);
+        let result = next_round(cavern, elf_attack_power);
         cavern = result.0;
         round_finished = result.1;
         if round_finished {
@@ -321,6 +352,17 @@ fn fight(mut cavern: Cavern) -> (Cavern, u32) {
         }
     }
     return (cavern, rounds);
+}
+
+fn cheat_until_elves_win(cavern: Cavern) -> (Cavern, u32, u32) {
+    let mut elf_attack_power: u32 = 4;
+    loop {
+        let (final_cavern, rounds) = fight(cavern.clone(), elf_attack_power);
+        if final_cavern.n_elves == cavern.n_elves {
+            return (final_cavern, rounds, elf_attack_power);
+        }
+        elf_attack_power += 1;
+    }
 }
 
 fn sum_health(cavern: &Cavern) -> u32 {
@@ -338,9 +380,14 @@ fn main() -> Result<(), String> {
     let lines: Vec<&str> = content.split('\n').collect();
     let initial_cavern = parse_cavern(&lines)?;
 
-    let (final_cavern, rounds) = fight(initial_cavern);
+    let (final_cavern, rounds) = fight(initial_cavern.clone(), 3);
     let health_sum = sum_health(&final_cavern);
     println!("Outcome of the battle: rounds: {}, remaining health: {}, surviving goblins: {}, surviving elves: {}, outcome value: {}", rounds, health_sum, final_cavern.n_goblins, final_cavern.n_elves, rounds * health_sum);
+
+    let (final_cheat_cavern, cheat_rounds, required_elf_attack_power) =
+        cheat_until_elves_win(initial_cavern.clone());
+    let cheated_health_sum = sum_health(&final_cheat_cavern);
+    println!("Elves win after {} rounds without losses, remaining health: {}. Required attack power: {}. Outcome value: {}", cheat_rounds, cheated_health_sum, required_elf_attack_power, cheat_rounds * cheated_health_sum);
 
     Ok(())
 }
@@ -444,11 +491,11 @@ mod test {
         let mut cavern = parse_cavern(lines).expect("Expected valid cavern");
 
         // when
-        cavern = next_round(cavern).0;
+        cavern = next_round(cavern, 3).0;
         println!("After 1 round:\n{}", cavern);
-        cavern = next_round(cavern).0;
+        cavern = next_round(cavern, 3).0;
         println!("After 2 rounds:\n{}", cavern);
-        cavern = next_round(cavern).0;
+        cavern = next_round(cavern, 3).0;
         println!("After 3 rounds:\n{}", cavern);
 
         // then
@@ -511,18 +558,252 @@ mod test {
         );
     }
 
+    #[test]
+    fn combat_works_for_example_3() {
+        let lines = &[
+            "#######", "#E..EG#", "#.#G.E#", "#E.##E#", "#G..#.#", "#..E#.#", "#######",
+        ];
+        run_combat_test(lines, 46, 859);
+    }
+
+    #[test]
+    fn combat_works_for_example_4() {
+        let lines = &[
+            "#######", "#E.G#.#", "#.#G..#", "#G.#.G#", "#G..#.#", "#...E.#", "#######",
+        ];
+        run_combat_test(lines, 35, 793);
+    }
+    #[test]
+    fn combat_works_for_example_5() {
+        let lines = &[
+            "#######", "#.E...#", "#.#..G#", "#.###.#", "#E#G#G#", "#...#G#", "#######",
+        ];
+        run_combat_test(lines, 54, 536);
+    }
+    #[test]
+    fn combat_works_for_example_6() {
+        let lines = &[
+            "#########",
+            "#G......#",
+            "#.E.#...#",
+            "#..##..G#",
+            "#...##..#",
+            "#...#...#",
+            "#.G...G.#",
+            "#.....G.#",
+            "#########",
+        ];
+        run_combat_test(lines, 20, 937);
+    }
+
     fn run_combat_test(raw_cavern: &[&str], expected_rounds: u32, expected_health: u32) {
         // given
         let initial_cavern = parse_cavern(raw_cavern).expect("Expected valid cavern");
 
         // when
         println!("initial cavern:\n{}", initial_cavern);
-        let (final_cavern, rounds) = fight(initial_cavern);
+        let (final_cavern, rounds) = fight(initial_cavern, 3);
         let health_sum = sum_health(&final_cavern);
         println!("final cavern:\n{}", final_cavern);
 
         // then
         assert_eq!(rounds, expected_rounds);
         assert_eq!(health_sum, expected_health);
+    }
+
+    #[test]
+    fn cheating_works_for_example_1() {
+        let lines = &[
+            "#######", "#.G...#", "#...EG#", "#.#.#G#", "#..G#E#", "#.....#", "#######",
+        ];
+        let cavern = run_cheating_test(lines, 29, 172, 15);
+
+        let elf1 = cavern.get_entity(3, 1).expect("Expected first elf");
+        assert_eq!(elf1.health, 158);
+
+        let elf2 = cavern.get_entity(4, 2).expect("Expected second elf");
+        assert_eq!(elf2.health, 14);
+    }
+
+    #[test]
+    fn cheating_works_for_example_2() {
+        let lines = &[
+            "#######", "#E..EG#", "#.#G.E#", "#E.##E#", "#G..#.#", "#..E#.#", "#######",
+        ];
+        run_cheating_test(lines, 33, 948, 4);
+    }
+
+    #[test]
+    fn cheating_works_for_example_3() {
+        let lines = &[
+            "#######", "#E.G#.#", "#.#G..#", "#G.#.G#", "#G..#.#", "#...E.#", "#######",
+        ];
+        run_cheating_test(lines, 37, 94, 15);
+    }
+
+    #[test]
+    fn cheating_works_for_example_4() {
+        let lines = &[
+            "#######", "#.E...#", "#.#..G#", "#.###.#", "#E#G#G#", "#...#G#", "#######",
+        ];
+        run_cheating_test(lines, 39, 166, 12);
+    }
+
+    #[test]
+    fn cheating_works_for_example_5() {
+        run_cheating_test(
+            &[
+                "#########",
+                "#G......#",
+                "#.E.#...#",
+                "#..##..G#",
+                "#...##..#",
+                "#...#...#",
+                "#.G...G.#",
+                "#.....G.#",
+                "#########",
+            ],
+            30,
+            38,
+            34,
+        );
+    }
+
+    fn run_cheating_test(
+        raw_cavern: &[&str],
+        expected_rounds: u32,
+        expected_health: u32,
+        expected_power: u32,
+    ) -> Cavern {
+        let initial_cavern = parse_cavern(raw_cavern).expect("Expected valid cavern");
+        let initial_elves = initial_cavern.n_elves;
+
+        // when
+        println!("initial cavern:\n{}", initial_cavern);
+        let (final_cavern, rounds, power) = cheat_until_elves_win(initial_cavern);
+        let health_sum = sum_health(&final_cavern);
+        println!("final cavern:\n{}", final_cavern);
+
+        // then
+        assert_eq!(final_cavern.n_goblins, 0);
+        assert_eq!(final_cavern.n_elves, initial_elves);
+        assert_eq!(rounds, expected_rounds);
+        assert_eq!(health_sum, expected_health);
+        assert_eq!(power, expected_power);
+
+        return final_cavern;
+    }
+
+    #[test]
+    fn test_attack_preferences_if_health_is_equal() {
+        // given
+        let initial_cavern = parse_cavern(&[".G.", "GEG", ".G."]).expect("Expected valid cavern");
+
+        // when
+        // 200 attack power, so each hit is a kill for elves
+        let (cavern1, _) = next_round(initial_cavern, 200);
+        println!("after round 1:\n{}", cavern1);
+        let (cavern2, _) = next_round(cavern1.clone(), 200);
+        println!("after round 2:\n{}", cavern2);
+        let (cavern3, _) = next_round(cavern2.clone(), 200);
+        println!("after round 3:\n{}", cavern3);
+        let (cavern4, _) = next_round(cavern3.clone(), 200);
+        println!("after round 4:\n{}", cavern4);
+
+        // then
+        assert_eq!(cavern1.get_entity(1, 0), None);
+        assert!(cavern1.get_entity(0, 1).is_some());
+        assert!(cavern1.get_entity(2, 1).is_some());
+        assert!(cavern1.get_entity(1, 2).is_some());
+
+        assert_eq!(cavern2.get_entity(1, 0), None);
+        assert_eq!(cavern2.get_entity(0, 1), None);
+        assert!(cavern2.get_entity(2, 1).is_some());
+        assert!(cavern2.get_entity(1, 2).is_some());
+
+        assert_eq!(cavern3.get_entity(1, 0), None);
+        assert_eq!(cavern3.get_entity(0, 1), None);
+        assert_eq!(cavern3.get_entity(2, 1), None);
+        assert!(cavern3.get_entity(1, 2).is_some());
+
+        assert_eq!(cavern4.get_entity(1, 0), None);
+        assert_eq!(cavern4.get_entity(0, 1), None);
+        assert_eq!(cavern4.get_entity(2, 1), None);
+        assert_eq!(cavern4.get_entity(1, 2), None);
+    }
+
+    #[test]
+    fn test_attack_preferences_for_lower_health() {
+        // given
+        let mut initial_cavern = parse_cavern(&["GEG"]).expect("Expected valid cavern");
+        initial_cavern.set(
+            2,
+            0,
+            Tile::Open(Some(Entity {
+                id: 2,
+                side: Side::Goblin,
+                health: 199,
+            })),
+        );
+
+        // when
+        // 200 attack power, so each hit is a kill for elves
+        let (cavern, _) = next_round(initial_cavern, 200);
+        println!("after round 1:\n{}", cavern);
+
+        // then
+        assert!(cavern.get_entity(0, 0).is_some());
+        assert_eq!(cavern.get_entity(0, 2), None);
+    }
+
+    #[test]
+    fn round_ends_prematurely_if_no_enemies_are_left() {
+        // given
+        let initial_cavern = parse_cavern(&["GEE"]).expect("Expected valid cavern");
+
+        // when
+        // 200 attack power, so each hit is a kill for elves
+        let (cavern_1, finished_round_1) = next_round(initial_cavern, 200);
+        let (cavern_2, finished_round_2) = next_round(cavern_1.clone(), 200);
+
+        // then
+        assert!(!finished_round_1);
+        assert!(!finished_round_2);
+        assert_eq!(cavern_1, cavern_2);
+    }
+
+    #[test]
+    fn round_does_not_end_prematurely_if_last_entities_turn_kills_last_enemy() {
+        // given
+        let initial_cavern = parse_cavern(&["EEG"]).expect("Expected valid cavern");
+
+        // when
+        // 200 attack power, so each hit is a kill for elves
+        let (cavern_1, finished_round_1) = next_round(initial_cavern, 200);
+        let (cavern_2, finished_round_2) = next_round(cavern_1.clone(), 200);
+
+        // then
+        assert!(finished_round_1);
+        assert!(!finished_round_2);
+        assert_eq!(cavern_1, cavern_2);
+    }
+
+    #[test]
+    fn movement_selection_with_tricky_obstacles_works_correctly() {
+        // given
+        // tricky, because the elf should select the tile left to the goblin
+        // as tile to attack from (first in reading order), but from the elf's position, up
+        // is the first tile in reading order
+        let lines = &[
+            "......#", ".####.#", "E####.#", ".##.G.#", ".##.###", ".##.###", ".......",
+        ];
+        let initial_cavern = parse_cavern(lines).expect("Expected valid cavern");
+
+        // when
+        let (cavern, _) = next_round(initial_cavern, 3);
+
+        // then
+        assert!(cavern.get_entity(0, 1).is_none());
+        assert!(cavern.get_entity(0, 3).is_some());
     }
 }
