7a0c61e6ee3c2f131904810a9454d3ca79ea6532
Guido Knips <guido.knips@posteo.de>
2018-12-23 20:41:07 +0100

    add solution for puzzle 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 
-fn geological_index(
+static EROSION_MOD: u64 = 20183;
+
+fn geologic_index(
     x: u64,
     y: u64,
     lookup_map: &mut HashMap<(u64, u64), u64>,
     target_x: u64,
     target_y: u64,
+    depth: u64,
 ) -> u64 {
     if x == 0 && y == 0 {
         return 0;
@@ -22,14 +25,12 @@ fn geological_index(
     if let Some(gi) = lookup_map.get(&(x, y)) {
         return *gi;
     }
-    let left = geological_index(x - 1, y, lookup_map, target_x, target_y);
-    let up = geological_index(x, y - 1, lookup_map, target_x, target_y);
+    let left = erosion_level(x - 1, y, lookup_map, target_x, target_y, depth);
+    let up = erosion_level(x, y - 1, lookup_map, target_x, target_y, depth);
     let gi = left * up;
     lookup_map.insert((x, y), gi);
     return gi;
 }
-
-static EROSION_MOD: u64 = 20183;
 
 fn erosion_level(
     x: u64,
@@ -39,7 +40,7 @@ fn erosion_level(
     target_y: u64,
     depth: u64,
 ) -> u64 {
-    let gi = geological_index(x, y, geo_map, target_x, target_y);
+    let gi = geologic_index(x, y, geo_map, target_x, target_y, depth);
     return (gi + depth) % EROSION_MOD;
 }
 
@@ -55,12 +56,38 @@ fn risk_level(depth: u64, target_x: u64, target_y: u64) -> u64 {
     return risk_level;
 }
 
+fn print_area(depth: u64, xmax: u64, ymax: u64) {
+    let mut geo_map: HashMap<(u64, u64), u64> = HashMap::with_capacity((xmax * xmax) as usize);
+    for y in 0..(ymax + 1) {
+        for x in 0..(xmax + 1) {
+            match erosion_level(x, y, &mut geo_map, xmax, ymax, depth) % 3 {
+                0 => {
+                    print!(".");
+                }
+                1 => {
+                    print!("=");
+                }
+                2 => {
+                    print!("|");
+                }
+                _ => {
+                    print!("\\033[1;31mE\\033[0m");
+                }
+            };
+        }
+        println!("");
+    }
+}
+
 fn main() -> Result<(), String> {
     // this stuff is specific for my input, but I did'nt want to bother with parsing
     // just two lines
     let depth: u64 = 8112;
     let (target_x, target_y): (u64, u64) = (13, 743);
     // input end
+
+    let ri = risk_level(depth, target_x, target_y);
+    println!("Risk level of the area is {}", ri);
 
     Ok(())
 }
@@ -80,6 +107,7 @@ mod test {
         let risk = risk_level(depth, tx, ty);
 
         // then
+        print_area(depth, tx, ty);
         assert_eq!(risk, 114);
     }
 }
